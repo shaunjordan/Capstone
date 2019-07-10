f1d0e79dbc130b738afa75dc0eafe5e1b2ab00ab
Shaun Jordan <s1bravo@gmail.com>
2019-07-10 14:11:16 -0500

    init commit

---
+++ Capstone/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Capstone/Models/Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Guard : Employee
+    {
+        public int BadgeNumber { get; set; }
+
+        public int PatrolCar { get; set; }
+    }
+}
